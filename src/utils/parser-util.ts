45892697cd02e18e7cdadcd654956ce8b298df85
streammx <streammx@naver.com>
2024-04-07 13:39:39 +0900

    mod: 유틸 클래스 정리

--- src/utils/parser-util.ts
+++ src/utils/parser-util.ts
@@ -3,9 +3,12 @@ import { checkObject, checkNumber } from "./validation-util";
 export const parseNumber = (data: any) =>
   /\./.test(data) ? parseFloat(data) : parseInt(data);
 
-export const parseDOM = (tagString: string) => {
+export const parseDOM = (
+  tagString: string,
+  type: DOMParserSupportedType = "text/html"
+) => {
   const parser = new DOMParser();
-  const doc = parser.parseFromString(tagString, "text/html");
+  const doc = parser.parseFromString(tagString, type);
   const element = doc.body.firstChild;
 
   return element;
